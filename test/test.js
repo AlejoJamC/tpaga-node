cf72ea5c4b44172299e02c5853373ab280ac5ab6
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-08-15 23:31:50 -0500

    working on test

--- test/test.js
+++ test/test.js
@@ -5,3 +5,11 @@
  * This source code is licensed under the MIT license found in the LICENSE
  * file in the root directory of this source tree or translated in the assets folder.
  */
+
+var assert  = require('assert'),
+    base64  = require('../lib/base64'),
+    tpaga   = require('../lib/tpaga');
+
+const LOCALE            = 'es',
+      PUBLIC_TEST_KEY   = 'pk_test_qvbvuthlvqpijnr0elmtg5jh',
+      PRIVATE_TEST_KEY  = 'd13fr8n7vhvkuch3lq2ds5qhjnd2pdd2';
